8436ad64cf14bdf6c430fc98d1caf777db374954
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2024-01-24 01:17:26 +0100

    Simple model with first tests

---
+++ src/main/java/pl/ks/it/graphql/graphqlserver/GraphqlServerApplication.java
@@ -0,0 +1,13 @@
+package pl.ks.it.graphql.graphqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlServerApplication.class, args);
+	}
+
+}
